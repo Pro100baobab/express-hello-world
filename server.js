8268028803c247238cf06cdf28e2c3665ba50050
Pro100baobab <159517950+Pro100baobab@users.noreply.github.com>
2025-04-16 02:13:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const jwt = require('jsonwebtoken');
 const cors = require('cors');
 
 const app = express();
+
+const PORT = process.env.PORT || 3001; // Render автоматически подставляет свой PORT
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 app.use(express.json());
 app.use(cors());
 
